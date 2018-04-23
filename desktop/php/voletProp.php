0cae7b6fc9bd77e4142a3ef5eb93d556101969eb
Halbout Michael <michaelhalbout@gmail.com>
2018-04-23 13:58:00 +0200

    Update voletProp.php

--- desktop/php/voletProp.php
+++ desktop/php/voletProp.php
@@ -41,20 +41,12 @@ $eqLogics = eqLogic::byType('voletProp');
 		<div class="eqLogicThumbnailContainer">
 			<?php
 				foreach ($eqLogics as $eqLogic) {
-					$opacity = '';
-					if ($eqLogic->getIsEnable() != 1) {
-						$opacity = '
-						-webkit-filter: grayscale(100%);
-						-moz-filter: grayscale(100);
-						-o-filter: grayscale(100%);
-						-ms-filter: grayscale(100%);
-						filter: grayscale(100%); opacity: 0.35;';
-					}
+					$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 					echo "<center>";
 					echo '<img src="plugins/voletProp/plugin_info/voletProp_icon.png" height="105" width="95" />';
 					echo "</center>";
-					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+					echo '<span class="name" style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
 					echo '</div>';
 				}
 			?>
