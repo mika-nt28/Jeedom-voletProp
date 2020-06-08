a18b316b44114726c569db16d46c5179b87735ca
Halbout Michael <michaelhalbout@gmail.com>
2020-06-08 11:04:00 +0200

    Create install.php

---
+++ plugin_info/install.php
@@ -0,0 +1,16 @@
+<?php
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+function voletProp_install(){
+}
+function voletProp_update(){
+	log::add('voletProp','debug','Lancement du script de mise a jours'); 
+	foreach(eqLogic::byType('voletProp') as $voletProp){
+		$voletProp->setConfiguration('TpsUp',$voletProp->getConfiguration('Ttotal'));
+		$voletProp->setConfiguration('TpsDown',$voletProp->getConfiguration('Ttotal'));
+		$voletProp->save();
+	}
+	log::add('voletProp','debug','Fin du script de mise a jours');
+}
+function voletProp_remove(){
+}
+?>
